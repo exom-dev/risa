ece2f40a06a363e2468fea1b90ee76db822a923f
UnexomWid <uw@exom.dev>
2020-07-11 19:48:51 +0300

    [REMOVE] Unused function header

--- src/compiler/parser.h
+++ src/compiler/parser.h
@@ -18,8 +18,6 @@ void parser_init(Parser* parser);
 void parser_advance(Parser* parser);
 void parser_consume(Parser* parser, TokenType type, const char* err);
 
-void parser_rule_get(TokenType type);
-
 void parser_error_at(Parser* parser, Token token, const char* msg);
 void parser_error_at_current(Parser* parser, const char* msg);
 void parser_error_at_previous(Parser* parser, const char* msg);
