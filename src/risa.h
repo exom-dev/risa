4f511746616390b09d006e25f051b898065b93fa
UnexomWid <uw@exom.dev>
2022-04-08 16:03:31 +0300

    add: extern c

--- src/risa.h
+++ src/risa.h
@@ -1,6 +1,10 @@
 #ifndef RISA_RISA_H_GUARD
 #define RISA_RISA_H_GUARD
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "api.h"
 #include "vm/vm.h"
 #include "std/std.h"
@@ -28,5 +32,9 @@ RISA_API RisaExecuteStatus   risa_execute_cluster   (RisaVM* vm, RisaCluster clu
 RISA_API RisaExecuteStatus   risa_execute_function  (RisaVM* vm, RisaDenseFunction* function);
 RISA_API RisaInterpretStatus risa_interpret_string  (RisaVM* vm, const char* str);
 RISA_API uint8_t*            risa_serialize_cluster (RisaCluster* cluster, uint32_t* size);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
